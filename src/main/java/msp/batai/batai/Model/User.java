5da5a9e197ea56f7b6d290f0292346c2bf975332
Mrudul Patil <mrudulsp.18@gmail.com>
2025-01-18 06:53:26 +0530

    updated User entity

--- src/main/java/msp/batai/batai/Model/User.java
+++ src/main/java/msp/batai/batai/Model/User.java
@@ -25,4 +25,12 @@ public class User {
     @Column(unique=true)
     private String username;
     private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number", length = 10)
+    private Long phoneNumber;
 }
